701a322d71a3c6dbc7c8102b31c5b6f47a5878fb
kforcool <1020396@student.sae.edu.au>
2021-02-11 23:04:22 +1000

    veriable commit

---
+++ Assets/Scripts/VeriableEtiquette.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// A script that shows the correct setup and usage of veriables inside unity
+/// </summary>
+public class VeriableEtiquette : MonoBehaviour
+{
+  
+    public int myFirstInt = 8; // An int stores whole numbers!
+    private int mySecondInt = 2;
+    public float myFirstFloat = 3.45f; // A float stores decimal numbers must have a f at the end to know its a float!
+    public double myFirstDouble = 2.34f; // Is the same as a float but can store up to 16 decimal numbers must use a f to Know its a double!
+    public bool myFirstBool = true; // Holds true or false statements!
+    public string myFirstString = "Keith is the man"; // Stores whole words must use "" to Know its a string
+    public char myFristChar = 'n'; // holds single charecters must use single '' to know its a char 
+
+        //first frame update
+    void Start()
+    {
+        myFirstString = "keith always is on time"; // sets first string to keith always is on time
+
+        myFirstInt = 10; // sets first int to 10
+    }        
+    }
+
